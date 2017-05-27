d73ffd41e0478d0b1a2aa043f213f8c5fd79b1af
liangshaojie <877942720@qq.com>
2017-05-27 21:32:27 +0800

    笔记本太难写了，暂停

---
+++ src/page/order/order.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>测试页面</div>
+</template>
+<script>
+  import {
+    mapActions, mapGetters
+  } from 'vuex'
+
+  export default {
+    components: {},
+    data(){
+      return {}
+    },
+    computed: {
+      ...mapGetters([]),
+    },
+    mounted(){
+
+    },
+    methods: {
+      ...mapActions([]),
+    },
+  }
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -17,96 +17,105 @@ const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder
 const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
 const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
 const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
-
+const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
 export default [{
-    path: '/',
-    component: App, //顶层路由，对应index.html
-    children: [ //二级路由。对应App.vue
-        //地址为空时跳转home页面
+  path: '/',
+  component: App, //顶层路由，对应index.html
+  children: [ //二级路由。对应App.vue
+    //地址为空时跳转home页面
+    {
+      path: '',
+      redirect: '/home'
+    },
+    //首页城市列表页
+    {
+      path: '/home',
+      component: home
+    },
+    //订单列表页
+    {
+      path: '/order',
+      component: order,
+      // children: [{
+      //   path: 'orderDetail', //订单详情页
+      //   component: orderDetail,
+      // },]
+    },
+    //当前选择城市页
+    {
+      path: '/city/:cityid',
+      component: city
+    },
+    //所有商铺列表页
+    {
+      path: '/msite',
+      component: msite,
+    },
+    //搜索页
+    {
+      path: '/search/:geohash',
+      component: search
+    },
+    //个人信息页
+    {
+      path: '/profile',
+      component: profile,
+    },
+    //特色商铺列表页
+    {
+      path: '/food',
+      component: food
+    },
+    {
+      path: '/shop',
+      component: shop,
+      children: [{
+        path: 'shopDetail', //商铺详情页
+        component: shopDetail,
+        children: [{
+          path: 'shopSafe', //商铺安全认证页
+          component: shopSafe,
+        },]
+      }, {
+        path: 'foodDetail', //食品详情页
+        component: foodDetail,
+      }]
+    },
+    //确认订单页
+    {
+      path: '/confirmOrder',
+      component: confirmOrder,
+      children: [
         {
-            path: '',
-            redirect: '/home'
-        },
-        //首页城市列表页
-        {
-            path: '/home',
-            component: home
-        },
-        //当前选择城市页
-        {
-            path: '/city/:cityid',
-            component: city
-        },
-        //所有商铺列表页
-        {
-            path: '/msite',
-            component: msite,
-        },
-        //搜索页
-        {
-            path: '/search/:geohash',
-            component: search
-        },
-        //个人信息页
-        {
-            path: '/profile',
-            component: profile,
-        },
-        //特色商铺列表页
-        {
-            path: '/food',
-            component: food
-        },
-        {
-            path: '/shop',
-            component: shop,
-            children: [{
-                path: 'shopDetail', //商铺详情页
-                component: shopDetail,
-                children: [{
-                    path: 'shopSafe', //商铺安全认证页
-                    component: shopSafe,
-                },]
-            }, {
-                path: 'foodDetail', //食品详情页
-                component: foodDetail,
-            }]
-        },
-        //确认订单页
-        {
-            path: '/confirmOrder',
-            component: confirmOrder,
-            children: [
+          path: 'chooseAddress', //选择地址
+          component: chooseAddress,
+          children: [
+            {
+              path: 'addAddress', //添加地址
+              component: addAddress,
+              children: [
                 {
-                    path: 'chooseAddress', //选择地址
-                    component: chooseAddress,
-                    children: [
-                        {
-                            path: 'addAddress', //添加地址
-                            component: addAddress,
-                            children:[
-                                {
-                                    path: 'searchAddress', //搜索地址
-                                    component: searchAddress,
-                                }
-                            ]
-                        }
-                    ]
-                },{
-                    path: 'remark', //订单备注
-                    component: remark,
-                },{
-                    path: 'invoice', //发票抬头
-                    component: invoice,
-                },{
-                    path: 'payment', //付款页面
-                    component: payment,
-                },{
-                    path: 'userValidation', //用户验证
-                    component: userValidation,
+                  path: 'searchAddress', //搜索地址
+                  component: searchAddress,
                 }
-            ]
+              ]
+            }
+          ]
+        }, {
+          path: 'remark', //订单备注
+          component: remark,
+        }, {
+          path: 'invoice', //发票抬头
+          component: invoice,
+        }, {
+          path: 'payment', //付款页面
+          component: payment,
+        }, {
+          path: 'userValidation', //用户验证
+          component: userValidation,
         }
+      ]
+    }
 
-    ]
+  ]
 }]
